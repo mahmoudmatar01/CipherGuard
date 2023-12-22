6714aeb2da6f336f85091f50228a3e950cef7a77
MahmoudMatar <mahmoudmatar49@gmail.com>
2023-12-22 16:40:22 +0200

    initial commit

--- lib/affine.dart
+++ lib/affine.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Affine extends StatelessWidget {
   const Affine({super.key});
 
-  int modInverse(int a, int m) {
-    for (int i = 0; i < m; i++) {
-      if ((a * i) % m == 1) {
-        return i;
-      }
-    }
-    throw Exception('Modular inverse does not exist');
-  }
-
-  // Encryption Method
-  // C = [ ( a * p ) + b ] % n
-  // key (a ,b)
+// Encryption Method
+// C = [ ( a * p ) + b ] % n
+// key (a ,b)
   String affineEncrypt(String plaintext, int keyA, int keyB) {
     StringBuffer ciphertext = StringBuffer();
 
@@ -43,11 +35,17 @@ class Affine extends StatelessWidget {
     return ciphertext.toString();
   }
 
-  // Decryption Method
-  // P = [ a^-1 * ( c- b ) ] % n
-  // key (a ,b)
+// Decryption Method
+// P = [ a^-1 * ( c- b ) ] % n
+// key (a ,b)
   String affineDecrypt(String ciphertext, int keyA, int keyB) {
     int modInverseA = modInverse(keyA, 26);
+
+    // Check if modular inverse exists
+    if (modInverseA == -1) {
+      throw Exception('Modular inverse does not exist');
+    }
+
     StringBuffer decryptedText = StringBuffer();
 
     for (int i = 0; i < ciphertext.length; i++) {
@@ -73,6 +71,52 @@ class Affine extends StatelessWidget {
     }
 
     return decryptedText.toString();
+  }
+
+// Extended Euclidean Algorithm to find modular inverse
+  List<int> extendedGCD(int a, int b) {
+    if (b == 0) {
+      return [a, 1, 0];
+    } else {
+      List<int> result = extendedGCD(b, a % b);
+      int d = result[0];
+      int x = result[2];
+      int y = result[1] - (a ~/ b) * result[2];
+      return [d, x, y];
+    }
+  }
+
+// Modular inverse function
+  int modInverse(int a, int m) {
+    List<int> result = extendedGCD(a, m);
+    int d = result[0];
+    int x = result[1];
+
+    if (d == 1) {
+      // Modular inverse exists
+      return (x % m + m) % m;
+    } else {
+      // Modular inverse does not exist
+      return -1;
+    }
+  }
+
+  int findCoprime(int n) {
+    Random random = Random();
+    int coprime;
+    do {
+      coprime = random.nextInt(n);
+    } while (gcd(coprime, n) != 1);
+    return coprime;
+  }
+
+  int gcd(int a, int b) {
+    while (b != 0) {
+      int temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return a;
   }
 
   @override
